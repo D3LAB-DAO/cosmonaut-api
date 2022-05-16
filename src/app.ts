2acd3df24683eeb5c9f02e9311bea284ce69a592
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2022-05-17 01:05:08 +0900

    chore: change the cors option depending on the mode

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import express from "express";
 import cors from "cors";
+
 import route from "./routes";
+import rustRoute from './routes/rust'
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.text());
 app.use(express.json());
-const whiteList = ['http://localhost:63342'];
-app.use(cors({origin: whiteList}));
+
+if (process.env.MODE === "test") {
+    app.use(cors())
+} else {
+    const whiteList = ['http://localhost:63342'];
+    app.use(cors({origin: whiteList}));
+}
+
 app.use('/', route);
+app.use('/rust', rustRoute)
 
 export default app
