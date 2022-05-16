0733770b349bf56ca0f6a08f84a20dd9f755f750
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2022-05-16 18:07:25 +0900

    chore: detach the test case for correct and incorrect cases

--- tests/rust.test.ts
+++ tests/rust.test.ts
@@ -1,51 +1,44 @@
 import {rust} from '../src/rust'
 
-test("test rustfmt",
-    async () => {
-        const targetCode: string = "fn main(){println!(\"test function\");}"
-        const wrongCode: string = "Hello I am not rust code"
-        const encodedData = Buffer.from(targetCode, "utf-8").toString('base64'); // encode a string
-        const encodedData2 = Buffer.from(wrongCode, "utf-8").toString('base64');
+describe("rustfmt test", () => {
+    test("with correct rust code",
+        async () => {
+            const targetCode: string = "fn main(){println!(\"test function\");}"
+            const encodedData = Buffer.from(targetCode, "utf-8").toString('base64'); // encode a string
 
-        try {
-            let res = await rust.fmt(encodedData)
-            res = Buffer.from(res, 'base64').toString('utf-8')
-            const cmp_str = "fn main() {\n" +
-                "    println!(\"test function\");\n" +
-                "}\n"
-            expect(res).toBe(cmp_str)
-            let res2 = await rust.fmt(encodedData2)
-        } catch(err) {
-            if (err instanceof  Buffer) {
-                err = err.toString()
-                expect(err).toMatch('error')
-            } else if(typeof err === "string") {
-                err = Buffer.from(err, 'base64').toString('utf-8')
-                console.error(err)
-                expect(err).toMatch('error')
-            } else {
-                expect(err).toMatch('error')
+            try {
+                let res = await rust.fmt(encodedData)
+                res = Buffer.from(res, 'base64').toString('utf-8')
+                const cmp_str = "fn main() {\n" +
+                    "    println!(\"test function\");\n" +
+                    "}\n"
+                expect(res).toBe(cmp_str)
+            } catch (err) {
+                if (err instanceof Buffer) {
+                    err = err.toString()
+                    expect(err).toMatch('error')
+                } else if (typeof err === "string") {
+                    err = Buffer.from(err, 'base64').toString('utf-8')
+                    expect(err).toMatch('error')
+                } else {
+                    expect(err).toMatch('error')
+                }
             }
-        }
-    })
+        })
 
-// (async function () {
-//     const targetCode: string = "fn main(){println!(\"test function\");}"
-//     const wrongCode: string = "Hello I am not rust code"
-//     const encodedData = Buffer.from(targetCode, "utf-8").toString('base64'); // encode a string
-//     const encodedData2 = Buffer.from(wrongCode, "utf-8").toString('base64');
-//     try {
-//         // let res = await rust.fmt(encodedData);
-//         // console.log(res);
-//         // console.log(Buffer.from(res, 'base64').toString('utf-8'));
-//         let res2 = await rust.fmt(encodedData2);
-//         console.log(res2);
-//         console.log(Buffer.from(res2, 'base64').toString('utf-8'));
-//     } catch(err) {
-//         if (err instanceof Buffer) {
-//             console.error(err.toString())
-//         } else {
-//             console.error(err)
-//         }
-//     }
-// })();
+    test("with wrong rust code",
+        () => {
+            const wrongCode: string = "Hello I am not rust code"
+            const encodedData = Buffer.from(wrongCode, "utf-8").toString('base64');
+            return rust.fmt(encodedData)
+                .catch(err => {
+                    let convToUTF8 = Buffer.from(err, 'base64').toString('utf-8')
+                    expect(convToUTF8).toMatch('error')
+                })
+
+        })
+})
+
+
+
+
